ce09ce7fb24179d1cb007c5f88f2dbd0e3200cf2
Kolesin Andrei <kolesinandrei1910@gmail.com>
2018-12-15 14:07:20 +0300

    added constructor and destructor

---
+++ tqueue.h
@@ -0,0 +1,26 @@
+#ifndef __TQUEUE_H
+#define __TQUEUE_H
+template<class T>
+class TQueue{
+	T* pMem;
+	int memSize;
+	int lpt,rptr,size;
+
+public:
+	TQueue(int maxSize = 10);
+	~TQueue();
+	void Push(T elem);
+	T Pop();
+	bool IsEmpty();
+	bool IsFull();
+
+};
+TQueue::TQueue(int maxSize){
+	pMem = new T[maxSize];
+	memSize = maxSize;
+	lptr = rptr = size = 0;
+}
+TQueue::~TQueue(){
+	delete[] pMem;
+}
+#endif
